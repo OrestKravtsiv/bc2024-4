9fd7980c0dbdb2e2c2855854184d457bc21a763b
Орест bc2024-4 <orest.kravtsiv@lnu.edu.ua>
2024-10-22 00:33:46 +0300

    I have added Out request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
+const superagent = require('superagent');
 const { program } = require('commander');
 
 //Налаштував потрібні аргументи
@@ -19,12 +20,26 @@ async function handleGetRequest(res, cacheFilePath) {
         res.writeHead(200, { 'Content-Type': 'image/jpeg' });
         res.end(data);
     } catch (error) {
-        if (error.code === 'ENOENT') {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
-        } else {
-            throw error;
-        }
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+}
+
+async function handlePutRequest(req, res, imagePath, httpCode) {
+    try {
+        const response = await superagent.get(`https://http.cat/${httpCode}`);
+        const image = response.body;
+        console.log("false")
+
+        await fs.writeFile(imagePath, image);
+
+        res.writeHead(201, { 'Content-Type': 'image/jpeg' });
+        res.end(image);
+    }
+    catch (error) {
+        console.error('Error saving image:', error);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error saving image');
     }
 }
 
@@ -32,13 +47,13 @@ async function handleGetRequest(res, cacheFilePath) {
 
 const requestListener = async function (req, res) {
     const urlPath = req.url.slice(1);
-    const cacheFilePath = path.join('cache', `${urlPath}.jpg`);
+    const cacheFilePath = path.join(options.cache, `${urlPath}.jpg`);
 
     try {
         if (req.method === 'GET') {
             await handleGetRequest(res, cacheFilePath);
         } else if (req.method === 'PUT') {
-            await handlePutRequest(req, res, cacheFilePath);
+            await handlePutRequest(req, res, cacheFilePath, urlPath);
         } else if (req.method === 'DELETE') {
             await handleDeleteRequest(res, cacheFilePath);
         } else {
